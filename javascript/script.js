94d57941ca7ee0462cae1f17e846593026619c36
GuWuelta <rm93449@fiap.com.br>
2022-05-26 18:12:23 -0300

    Checkpoint 3 - HTML, CSS e JavaScript criados.

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+const botaoSubmit = document.getElementById("btnEnviar")
+
+function validaCampos(){
+
+    
+    const campos = document.querySelectorAll("input[type='text'],input[type='email'],input[type='date']")
+    
+    for(let x = 0; x < campos.length ; x++ ){
+        if(campos[x].value == ''){
+            alert('O CAMPO : ' + campos[x].name + ' não foi preenchido!')
+            return
+        }
+    }
+
+    for(let x = 0; x < campos.length ; x++ ){
+        if(campos[x].value != ''){
+            alert('O cadastro foi realizado com sucesso!')
+            window.location.href = './index.html'
+        }
+    }
+    
+    
+    document.querySelector(".cadastro").submit()
+}
+
+
+botaoSubmit.addEventListener("click", validaCampos)
